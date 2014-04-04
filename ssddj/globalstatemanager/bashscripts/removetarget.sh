abb8ba7b905abf52e00a00c4d0b8b0af7fcaa541
Sachin Agarwal,Orion dev machine <sacagarwal@blackberry.com>
2014-04-04 18:48:45 +0000

    Delete functionality implemented, including aagroup removal. Todo wrt delete is provisioner object removal

---
+++ ssddj/globalstatemanager/bashscripts/removetarget.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+LVOL=`scstadmin -list_target $1  -driver iscsi | grep disk-lvol | cut -d"-" -f2`
+yes | scstadmin -rem_target $1 -driver iscsi
+scstadmin -write_config /etc/scst.conf
+lvremove -f /dev/storevg/$LVOL
+
+
