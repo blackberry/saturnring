e6716bce6115746d6e1d687523a43c64c0fe278f
Sachin Agarwal,Orion dev machine <sacagarwal@blackberry.com>
2014-05-09 18:37:05 +0000

    Fixed target remove bug - now removing device as well

--- ssddj/globalstatemanager/bashscripts/removetarget.sh
+++ ssddj/globalstatemanager/bashscripts/removetarget.sh
@@ -4,6 +4,8 @@ then
         yes | scstadmin -rem_target $1 -driver iscsi
         TARGETMD5=`echo $1 | md5sum | cut -f1 -d" "`
         lvolName=lvol-${TARGETMD5:0:8}
+        lvu=`lvdisplay $2/$lvolName | grep "LV UUID" | sed  's/LV UUID\s\{0,\}//g' | tr -d '-' | tr -d ' '`
+        scstadmin -close_dev disk-${lvu:0:8} -handler vdisk_blockio
         echo "Trying to remove LV "$lvolName
         yes | lvremove -f $2/$lvolName
         scstadmin -write_config /etc/scst.conf
