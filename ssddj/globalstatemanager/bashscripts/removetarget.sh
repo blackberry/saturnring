a3ee44a994db2f55833d590deed76d3fd504c6b3
Sachin Agarwal,Orion dev machine <sacagarwal@blackberry.com>
2014-05-09 20:02:03 +0000

    Fixed remove target bug

--- ssddj/globalstatemanager/bashscripts/removetarget.sh
+++ ssddj/globalstatemanager/bashscripts/removetarget.sh
@@ -5,7 +5,7 @@ then
         TARGETMD5=`echo $1 | md5sum | cut -f1 -d" "`
         lvolName=lvol-${TARGETMD5:0:8}
         lvu=`lvdisplay $2/$lvolName | grep "LV UUID" | sed  's/LV UUID\s\{0,\}//g' | tr -d '-' | tr -d ' '`
-        scstadmin -close_dev disk-${lvu:0:8} -handler vdisk_blockio
+        yes | scstadmin -close_dev disk-${lvu:0:8} -handler vdisk_blockio
         echo "Trying to remove LV "$lvolName
         yes | lvremove -f $2/$lvolName
         scstadmin -write_config /etc/scst.conf
