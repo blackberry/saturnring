5928e54712a90e577faa548635cc22d17b9e28b8
Sachin Agarwal,Orion dev machine <sacagarwal@blackberry.com>
2014-05-14 13:23:24 +0000

    Fixed thinlvstats output order

--- ssddj/globalstatemanager/bashscripts/thinlvstats.sh
+++ ssddj/globalstatemanager/bashscripts/thinlvstats.sh
@@ -1,5 +1,4 @@
 #lvs --units g | grep "\s\sthinpool*" | cut -d" " -f6- | tr -d " " | cut -d"g" -f2
 #lvs --units g | grep "\s\sthinpool*" | cut -d" " -f6- | tr -d " " | cut -d"g" -f1
-lvdisplay storevg/thinpool --units g | grep "LV Size" | sed 's/[^0-9\.]*//g'
 lvdisplay storevg/thinpool --units g | grep "Allocated pool data" | sed 's/[^0-9\.]*//g'
-
+lvdisplay storevg/thinpool --units g | grep "LV Size" | sed 's/[^0-9\.]*//g'
