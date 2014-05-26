2a59e271788f4711827de195073562e4859463fc
Sachin Agarwal <sacagarwal@blackberry.com>
2014-05-26 09:55:16 -0400

    Fixed dir in rqworker

--- misc/rqworker.sh
+++ misc/rqworker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/vagrant/saturnring/godjango/bin/activate
+source /home/vagrant/saturnring/saturnenv/bin/activate
 python /home/vagrant/saturnring/ssddj/manage.py rqworker default
