bc7d3bc814b7ce6b0ee6fba44ac45b500a5c536f
Sachin Agarwal <sachinkagarwal@gmail.com>
2014-05-28 00:01:11 -0400

    Update saturnring_postbootup_as_vagrant_user.sh

--- deployments/vagrant/saturnring_postbootup_as_vagrant_user.sh
+++ deployments/vagrant/saturnring_postbootup_as_vagrant_user.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 #Copyright 2014 Blackberry Limited
 #
 #Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,7 +13,7 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-#!/bin/bash
+
 cd /home/vagrant
 git clone https://github.com/sachinkagarwal/saturnring/
 
