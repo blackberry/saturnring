7d143889f77e08c50a0eaed77182ed6036cfb33a
Sachin Agarwal <sachinkagarwal@gmail.com>
2014-06-02 03:30:17 +0000

    Fixed permissions for the targethistory tables

--- ssddj/ssdfrontend/admin.py
+++ ssddj/ssdfrontend/admin.py
@@ -57,6 +57,24 @@ class TargetHistoryAdmin(StatsAdmin):
     stats=(Sum('sizeinGB'),Sum('rkb'),Sum('wkb'))
     actions=[]
 
+    def has_change_permission(self, request, obj=None):
+        has_class_permission = super(TargetHistoryAdmin, self).has_change_permission(request, obj)
+        if not has_class_permission:
+            return False
+        if obj is not None and not request.user.is_superuser and request.user.id != obj.owner.id:
+            return False
+        return True
+
+    def queryset(self, request):
+        if request.user.is_superuser:
+            return TargetHistory.objects.all()
+        return TargetHistory.objects.filter(owner=request.user)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.owner = request.user
+        obj.save()
+
 admin.site.register(TargetHistory,TargetHistoryAdmin)
 
 class TargetAdmin(StatsAdmin):
