22099b292aebace7275e045be0959a3bc18a0884
Sachin Agarwal <sacagarwal@blackberry.com>
2015-03-31 09:30:06 -0400

    Added available and max LUN sizes, added site title

--- ssddj/ssdfrontend/templatetags/clusterinfo.py
+++ ssddj/ssdfrontend/templatetags/clusterinfo.py
@@ -3,6 +3,7 @@ from django import template
 from ssdfrontend.models import StorageHost
 from ssdfrontend.models import VG
 from django.db.models import Sum
+from django.db.models import Max 
 register = template.Library()
 
 @register.simple_tag
@@ -13,9 +14,13 @@ def get_clusterinfo():
             numHosts = 0
 
         totalCap =  VG.objects.all().aggregate(totalGB=Sum('totalGB'))['totalGB']
+        avlCap = VG.objects.all().aggregate(totalavl=Sum('maxavlGB'))['totalavl']
+        maxSize = VG.objects.all().aggregate(maxLUN=Max('maxavlGB'))['maxLUN']
         if totalCap == None:
-            totalCap = 0
-        return "%d saturn servers with total capacity  %d GB." %(numHosts,totalCap)
+            totalCap = -9999999999
+        if (avlCap == None) or (avlCap < 0):
+            avlCap = -99999999999
+        return "Saturn servers: %d; Total/Available/Max LUN capacity: %d/%d/%d GB" %(numHosts,totalCap,avlCap,maxSize)
     except:
         return 'Undefined'
         
